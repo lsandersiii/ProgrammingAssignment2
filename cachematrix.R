8fc11194e0e2348a62d8e72f4e1242c8844d9528
lsandersiii <lsandersiii@earthlink.net>
2022-04-25 18:17:23 -0700

    Wk3 Assignment
    R Programming Assignment Week 3 from Lonnie Sanders - peer graded assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(vec_mat = matrix()) {
+  mat_inv <- NULL   # set value of inverse from cachesolve()function to vec_mat
+  
+  set <- function(y) {  # function to set parameter of makeCacheMatrix() function & reset vec_mat from cachesolve
+    vec_mat <<- y
+    mat_inv <<- NULL
+  }
+ 
+  get <- function() vec_mat
+  setinv <- function(inv1) mat_inv <<- inv1   # assign value of inverse to vec_mat in the parent scope
+  getinv <- function() mat_inv
+  #list(set = set, get = get, setinv = setinv, getinv = getinv)
+  list(set = set,           # gives the name 'set' to the set() function defined above
+       get = get,           # gives the name 'get' to the get() function defined above
+       setinv = setinv,     # gives the name 'setinv' to the setinv() function defined above
+       getinv = getinv)     # gives the name 'getinv' to the getinv() function defined above
 }
 
 # cacheSolve: This function computes the inverse of the special "matrix" returned by 
