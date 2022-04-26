062e58feb30ef7fe4c44eaf02a131607b1ad7aa3
lsandersiii <lsandersiii@earthlink.net>
2022-04-25 17:42:21 -0700

    Update cachematrix.R
    R Programming Assignment Week 3 from Lonnie Sanders - peer graded assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -7,9 +7,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
-## Write a short comment describing this function
-
+# cacheSolve: This function computes the inverse of the special "matrix" returned by 
+# makeCacheMatrix above.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		 mat_inv <- vec_mat$getinv()
+  if(!is.null(mat_inv)) {
+    message("getting cached data")
+    return(mat_inv)
+  }
+  data <- vec_mat$get()
+  mat_inv <- solve(data, ...)
+  vec_mat$setinv(mat_inv)
+  mat_inv
 }
